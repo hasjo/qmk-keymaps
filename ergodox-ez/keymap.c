4048b56ac0f2e3ea854d78aff1a72bbeaaf2cdfd
hasjo <jorhaskovec@protonmail.com>
2024-07-23 17:53:13 -0500

    make the ergodox a smidge more like my redox layout

--- ergodox-ez/keymap.c
+++ ergodox-ez/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_EQL,  KC_Q,   KC_W,       KC_E,    KC_R,      KC_T,    TG(SYMB),             TG(SYMB),     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
    KC_LALT, KC_A,   KC_S,       KC_D,    KC_F,      KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, GUI_T(KC_QUOT),
    KC_LSFT, KC_Z,   KC_X,       KC_C,    KC_V,      KC_B,    ALL_T(KC_NO),         MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-            KC_GRV, KC_QUOT,    KC_LCTL, KC_LEFT,   KC_RGHT,                                     KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, KC_RCTL,
+            KC_GRV, KC_QUOT,    KC_LEFT, KC_RGHT,   KC_LCTL,                                     KC_RCTL, KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,
                                                     KC_HOME, KC_END,               KC_PGUP,      KC_PGDN,
                                                              KC_DEL,               KC_INS,
                                             KC_SPC, KC_BSPC, KC_PSCR,              KC_PAUS,      KC_TAB, KC_ENT
